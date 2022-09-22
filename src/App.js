34594bb21ab47dd716c263c4ea80d96c1e4eb8ae
Jonathan Elias Dawlabani <46453179+jdawlabani@users.noreply.github.com>
2022-09-22 19:03:56 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  useEffect(()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+    .then(res=>{setPokemon(res.data.results)})
+    // .then(res=>console.log(res.data.count))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pokemon}
     </div>
   );
 }
